467e45c4092b784e40eaad67186d620d29ee5e40
gasparmartinez.dev <gasparmartinez.dev@gmail.com>
2026-09-03 19:37:04 +0200

    Calculo del factor de balanceo (closes #7)

--- src/train.py
+++ src/train.py
@@ -8,4 +8,12 @@ df = cargar_datos("data/creditcard.csv")
 
 X_train, X_test, y_train, y_test = separar_escalar_datos(df, ruta_scaler="models/scaler.joblib")
 
-print(X_train[:5])
+# print(X_train[:5])
+
+# Cálculo del factor de balanceo
+
+num_neg = (y_train == 0).sum()
+num_pos = (y_train == 1).sum()
+factor_balanceo = num_neg / num_pos
+
+print(f"El factor de balanceo es: {factor_balanceo}")
